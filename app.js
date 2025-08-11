55a18f0379c2f7a780878f395d8ecc28599b02a5
Isaac-Sands <isaacjamessands@gmail.com>
2025-08-10 20:53:49 -0500

    Add cpu choice function to app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+let computerChoice;
+let playerChoice;
 // get computer choice
-
+getComputerChoice()
+console.log(computerChoice)
 // get user choice
 
 // determine winner
@@ -9,3 +12,19 @@
 // compare score to winning round number
 
 // reset?
+
+//functions
+function getComputerChoice() {
+    const min = Math.ceil(1);
+    const max = Math.floor(4);
+    computerChoice = Math.floor(Math.random() * (max - min) + min);
+    if (computerChoice === 1) {
+        computerChoice = "Rock";
+    }
+    else if (computerChoice === 2) {
+        computerChoice = "Paper"
+    }
+    else if (computerChoice === 3) {
+        computerChoice = "Scissors"
+    }
+}
