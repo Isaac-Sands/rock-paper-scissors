fb9de746105c30cb907887dfe7024e02a1cd2294
Isaac-Sands <isaacjamessands@gmail.com>
2025-08-10 21:39:28 -0500

    Add playRound function update other functions

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let computerChoice;
 let playerChoice;
+let computerScore = 0;
+let playerScore = 0;
 // get computer choice
 getPlayerChoice();
 getComputerChoice();
 console.log(playerChoice);
 console.log(computerChoice);
+playRound(playerChoice, computerChoice);
 // get user choice
 
 // determine winner
@@ -21,20 +24,45 @@ function getComputerChoice() {
     const max = Math.floor(4);
     computerChoice = Math.floor(Math.random() * (max - min) + min);
     if (computerChoice === 1) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
         return computerChoice;
     }
     else if (computerChoice === 2) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
         return computerChoice;
     }
     else if (computerChoice === 3) {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
         return computerChoice;
     }
 }
 
 function getPlayerChoice() {
     playerChoice = prompt("Rock, Paper or Scissors?");
+    playerChoice = playerChoice.toLowerCase();
     return playerChoice;
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+    else if (playerChoice === "paper" && computerChoice === "rock") {
+        console.log("Paper beats Rock, you win!");
+    }
+    else if (playerChoice === "paper" && computerChoice === "scissors") {
+        console.log("Scissors beats Paper, you lose!");
+    }
+    else if (playerChoice === "scissors" && computerChoice === "paper") {
+        console.log("Scissors beats Paper, you win!");
+    }
+    else if (playerChoice === "scissors" && computerChoice === "rock") {
+        console.log("Rock beats Scissors, you lose!");
+    }
+    else if (playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("Rock beats Scissors, you win!");
+    }
+    else if (playerChoice === "rock" && computerChoice === "paper") {
+        console.log("Paper beats Rock, you lose!");
+    }
 }
